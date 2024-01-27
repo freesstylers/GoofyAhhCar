60e34f6dddcc29c08eb2f0bc4b63e0cde421c8b1
Jose Martin Serrano <josemsdev@gmail.com>
2024-01-27 20:23:28 +0100

    NPC Perso

---
+++ clusy-cart/Assets/Prefabs/NPC/Preso/PresoNPC.gd
@@ -0,0 +1,26 @@
+class_name PresoNPC
+extends NPC
+
+@export_category("Interval Min Max")
+@export var min :float = 1
+@export var max :float = 5
+
+var canMove = true
+var timeWaitingInterval : Vector2 = Vector2()
+var timer : Timer = null
+
+func _ready():
+	super._ready()
+	timer = Timer.new()
+	add_child(timer)
+	timer.timeout.connect(togle_move)
+	var waitTime = randf_range(min, max)
+	timer.start(waitTime)
+
+func move(delta):
+	if canMove:
+		super.move(delta)
+		
+
+func togle_move():
+	canMove = not canMove
