f20871a2773195f354ca9630f71600bd7a41a1a6
GCidon <gcidonch@ucm.es>
2024-01-27 11:05:17 +0100

    El player el player

--- clusy-cart/Assets/Scripts/Player/PlayerMovement.gd
+++ clusy-cart/Assets/Scripts/Player/PlayerMovement.gd
@@ -1,126 +1,96 @@
-extends RigidBody2D
+extends CharacterBody2D
 class_name Player
-
-@export var ACCELERATION := 1000.0
-@export var MAX_SPEED := 400.0
-
-@export var ANGULAR_ACCELERATION := 50.0
-@export var MAX_ANGULAR_VELOCITY := 5.0  
-
-#@export var ANGULAR_ANGLES := 5.0
-
-
-#BOOST RELATED
-var boostTimer : Timer = null
-var driftingTimer : Timer = null
-var boostAvailable : bool = false
-
-#POWERUP RELATED
-var betterHandling : bool = false
-
-var turningRight : bool = false
-var turningLeft : bool = false
-var used_acceleration : float = 0
-var used_andular_acceleration : float = 0
 
 var imagen : Sprite2D = null
 
-var speed : float = 0.0
+###########################################NUEVO SISTEMA
+@export var wheel_base : float = 70  # Distance from front to rear wheel
+@export var steering_angle : float = 15  # Amount that front wheel turns, in degrees
 
-func _ready():
-	boostTimer = $BoostTimer
-	driftingTimer = $DriftingTimer
-	
+@export var steer_angle : float = 0.0
+@export var engine_power : float = 800  # Forward acceleration force.
+
+var acceleration : Vector2 = Vector2.ZERO
+@export var friction : float = -0.9
+@export var drag : float = -0.0015
+@export var braking : float = -450
+@export var max_speed_reverse : float = 250
+@export var slip_speed : float = 400  # Speed where traction is reduced
+@export var traction_fast : float = 0.1  # High-speed traction
+@export var traction_slow : float = 0.7  # Low-speed traction
+###############################################
+
+var maxVelGotten : bool = false
+
+func _ready():	
 	Globals.ThePlayer = self
-	Globals.player_death.connect(PlayerDeath)
 	Globals.game_init_everything.connect(on_game_init_everything)
-	used_acceleration = ACCELERATION
-	used_andular_acceleration = ANGULAR_ACCELERATION
 	
 func _process(_delta):
-	
-	if Input.is_action_pressed("up"):
-		if speed <= 1.0:
-			speed+=0.1
-	else:
-		if speed >= 0.1:
-			speed-=0.1
-	if Input.is_action_pressed("back"):
-		if speed >= -1.0:
-			speed -= 0.1
-	else:
-		if speed <= -0.1:
-			speed += 0.1
-	if Input.is_action_pressed("right") or Input.is_action_pressed("left"):
-		if Input.is_action_pressed("right"):
-			turningRight = true
-			turningLeft = false
-		elif Input.is_action_pressed("left"):
-			turningRight = false
-			turningLeft = true
-	else:
-		turningRight = false
-		turningLeft = false
-		
-	
-	var steering_input = 0
-	if turningRight and not turningLeft:
-		steering_input = 1.0
-	if turningLeft and not turningRight:
-		steering_input = -1.0
-
-	# Rotate the body based on the steering input
-	apply_torque(used_andular_acceleration * steering_input)
-	
-	if abs(angular_velocity) > MAX_ANGULAR_VELOCITY:
-		set_angular_velocity(sign(angular_velocity) * MAX_ANGULAR_VELOCITY)
-		#angular_velocity = sign(angular_velocity) * MAX_ANGULAR_VELOCITY
-	
-	#Accelerate towards the direction the player is facing
-	var steering_direction = Vector2(1.0, 0).rotated(rotation)
-	
-	if linear_velocity.length() > MAX_SPEED:
-		linear_velocity = linear_velocity.normalized() * MAX_SPEED
-		
-	apply_force(steering_direction * used_acceleration * speed)
-
-#Rotate the player so that it "bounces" away from the wall
-func PlayerCollidedWithWall(wall:Node2D):
-	linear_velocity = linear_velocity.bounce((Vector2((get_viewport().size/2)) - wall.global_position).normalized()) 
-	look_at(global_position + linear_velocity)
-	
-func StartBoosting():
-	boostTimer.start()
-	Globals.player_boosting_state_changed.emit(true)
-	
-func StopBoosting():
-	used_acceleration = ACCELERATION
-	Globals.player_boosting_state_changed.emit(false)
-	
-func BoosReady():
-	boostAvailable = true
-
-#####GLOBAL SIGNALS#####
-
-func PlayerDeath():
-#	GlobalConstants.ThePlayer = null
-#	queue_free()
-	linear_velocity = Vector2(0,0)
-	angular_velocity = 0
-	used_acceleration = 0
-	used_andular_acceleration = 0
-	
-func on_power_up_chosen(PowerUp : Globals.PowerUp):
-	#match PowerUp:
-		#Globals.PowerUp.BETTER_HANDLING:
-			#used_andular_acceleration = BETTER_ANGULAR_ACCELERATION
 	pass
 			
+func get_input():
+	var turn = 0
+	if Input.is_action_pressed("right"):
+		turn += 1
+	if Input.is_action_pressed("left"):
+		turn -= 1
+	steer_angle = turn * steering_angle
+	if Input.is_action_pressed("up"):
+		#TODO SONIDO
+		acceleration = transform.x * engine_power
+	if Input.is_action_pressed("back"):
+		acceleration = transform.x * braking
+		
+func _physics_process(delta):
+	acceleration = Vector2.ZERO
+	get_input()
+	apply_friction()
+	calculate_steering(delta)
+	velocity += acceleration * delta
+	
+	var collision = move_and_collide(velocity*delta)
+	if collision:
+		#TODO SONIDO
+		velocity = velocity.bounce(collision.get_normal())
+
+func apply_friction():
+	if velocity.length() < 5:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction
+	var drag_force = velocity * velocity.length() * drag
+	if velocity.length() < 100:
+		friction_force *= 3
+	
+	acceleration += drag_force + friction_force
+	#Cuando llega a velocidad maxima hace sonido, y hasta que no vuelve a llegar no vuelve a sonar
+	if acceleration.length() < 0.1:
+		if not maxVelGotten: 
+			#TODO SONIDO
+			maxVelGotten = true
+	else: 
+		maxVelGotten = false
+		
+	
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_angle) * delta
+	var new_heading = (front_wheel - rear_wheel).normalized()
+	var traction = traction_slow
+	if velocity.length() > slip_speed:
+		traction = traction_fast
+	var d = new_heading.dot(velocity.normalized())
+	if d > 0:
+		velocity = velocity.lerp(new_heading * velocity.length(), traction)
+	if d < 0:
+		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+	rotation = new_heading.angle()
+
+#####GLOBAL SIGNALS#####
+			
 func on_game_init_everything():
-	used_acceleration = ACCELERATION
-	used_andular_acceleration = ANGULAR_ACCELERATION
-	rotation = 0
-	boostAvailable = false
 	#Position the player right in the middle of the screen
 	var screen_size = get_viewport().size
 	var center_screen = Vector2(screen_size.x / 2, screen_size.y / 2)
