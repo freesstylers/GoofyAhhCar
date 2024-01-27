9905f8c1598d3b91338ce6742ae98314e333d8a0
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2024-01-27 10:06:38 +0100

    Ajustes en el movimiento del npc

--- clusy-cart/Assets/Prefabs/NPC/NormalNpcMovement.gd
+++ clusy-cart/Assets/Prefabs/NPC/NormalNpcMovement.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var points : int = 100
 @export var bonus :String = ""
 @export var speed := 300.0
+@export var distanceThreshold := 20
 
 var goTo: Node2D = null
 var pointBehaviour : PointsB = null
@@ -13,14 +14,14 @@ func _ready():
 	goTo = pointBehaviour.get_actual_point()
 
 func is_min_dist(d):
-	return position.distance_to(goTo.position) <= d
+	return global_position.distance_to(goTo.global_position) <= d
 	
 	
 func move(delta):
-	var dir = (goTo.position - position).normalized()
-	position = position + dir*speed*delta
-	
-	if is_min_dist(1):
+	var dir = (goTo.global_position - global_position).normalized()
+	global_position = global_position + dir*speed*delta
+	look_at(goTo.global_position)
+	if is_min_dist(distanceThreshold):
 		goTo = pointBehaviour.get_next_point()
 
 
