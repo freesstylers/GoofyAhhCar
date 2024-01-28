72a96a73bf5be522ff26b428ff6b1b3643d0af95
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2024-01-28 10:48:25 +0100

    muerte de NPC

--- clusy-cart/Assets/Prefabs/NPC/NormalNpcMovement.gd
+++ clusy-cart/Assets/Prefabs/NPC/NormalNpcMovement.gd
@@ -15,6 +15,7 @@ func setReady():
 	goTo = pointBehaviour.get_actual_point()
 	global_position = goTo.global_position
 
+
 func is_min_dist(d):
 	return global_position.distance_to(goTo.global_position) <= d
 	
@@ -35,8 +36,7 @@ func _process(delta):
 		move(delta)
 
 func die():
-	free()
-	pass
+	queue_free()
 
 func spawn():
 	pass
@@ -45,3 +45,16 @@ func on_collision(other):
 	if other.is_in_group("player"):
 		Globals.npc_hit.emit(npcType)
 		die()
+		#goTo = null
+		#get_parent().remove_child(self)
+		#other.add_child(self)
+		#position = Vector2(0,0)
+		#
+		#var animLength : float = 2
+		#var randomDir = randf() * 2*PI
+		#var dirToMove : Vector2 = Vector2(1,0).rotated(randomDir)
+		#var localTween : Tween = create_tween()
+		#localTween.set_parallel(true)
+		#localTween.tween_property(self, "position", Vector2(Globals.WINDOW_BASE_SIZE.x,0), animLength)
+		#localTween.tween_property(self, "scale", Vector2(3,3), animLength)
+		#localTween.tween_callback(self.queue_free).set_delay(animLength)
